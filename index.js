97131b69ede0da5daef653b1a3e8f6edb93e3f65
Nishant Racherla <nishant.racherla@gmail.com>
2023-03-08 17:41:41 +0530

    added schema for user

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
+const User = require("./models/User");
+const Post = require("./models/Post");
+const expressGraphQL = require("express-graphql").graphqlHTTP;
+const schema = require("./schema/schema");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully."))
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
 app.use(bodyParser.json());
+app.use("/graphql", expressGraphQL({ schema, graphiql: true }));
+
 app.listen(5000, () => console.log("Server is running on port 5000"));
